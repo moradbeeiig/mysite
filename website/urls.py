b6d642570721362b64e8b79229d977297477894c
masoud rezaei <86180403+moradbeeiig@users.noreply.github.com>
2023-06-16 16:18:50 +0430

    blog
    blog post added by statrtapp and making viwes and urls in blog and making urls in main viwe

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from website.views import *
 
+app_name= 'website'
+
 urlpatterns = [
-    path('',index_views),
-    path('About',about_views),
-    path('Contacts',contact_views)
+    path('',index_views,name="index"),
+    path('About',about_views,name='about'),
+    path('Contacts',contact_views,name='contact')
 ]
