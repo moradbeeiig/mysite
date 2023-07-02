2f6745ee7034fdcf254edc368db5b5e788c85a8f
masoud rezaei <86180403+moradbeeiig@users.noreply.github.com>
2023-07-02 21:23:03 +0430

    chapter 7 all blog tags and paginations
    blog tags and paginations

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ app_name= 'blog'
 urlpatterns = [
     path('',blog_views,name="index"),
     path('<int:pid>',blog_single,name="single"),
+    path('category/<str:cat_name>',blog_category,name="category"),
+    path('author/<str:author_username>',blog_views,name='author'),
+    path('search/',blog_search,name='search'),
     path('test',test,name="test")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 # Create your views here.
-def blog_views(request):
+def blog_views(request,**kwargs):    #**kwrgs=cat_name=None,author_username=None
     posts=Post.objects.filter(status=1)
+    if kwargs.get('cat_name') !=None:
+        posts=posts.filter(category__name = kwargs['cat_name'])
+    if kwargs.get('author_username'):
+        posts = posts.filter(author__username = kwargs['author_username'])
+
+    posts=Paginator(posts,3)
+    try:
+        page_number=request.GET.get('page')
+        posts=posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts=posts.get_page(1)
+    except EmptyPage:
+        posts=posts.get_page(1)      
     contex={'posts':posts}
     return render(request,'blog/blog-home.html',contex)
 
@@ -17,3 +31,19 @@ def test(request):
     # post=get_object_or_404(Post,pk=pid)
     # contex={'post':post}
     return render(request,'test.html')
+
+def blog_category(request,cat_name):
+     posts=Post.objects.filter(status=1)
+     posts=posts.filter(category__name=cat_name)
+     contex={'posts':posts}
+     return render(request,'blog/blog-home.html',contex)
+
+def blog_search(request):
+    posts=Post.objects.filter(status=1)
+    if request.method=='GET':
+        if s:= request.GET.get('s'):
+            posts=posts.filter(content__contains=s)
+    contex={'posts':posts}
+    return render(request,'blog/blog-home.html',contex)
+
+     
